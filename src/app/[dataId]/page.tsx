cdee1632fa5e453a8c609007962f49e6f5cd585b
Shreyash Fulzele <shreyashfulzele@la-siriuss-mac.local>
2024-03-24 15:59:00 +0530

    vercel

--- src/app/[dataId]/page.tsx
+++ src/app/[dataId]/page.tsx
@@ -29,7 +29,7 @@ const DetailPage: any = ({params}: {params: {dataId: string}}) => {
 			  if(criteria.variable[item].type == 'value') {
 				modified = modified.replace(item, `<a href="https://fitpage-revised.vercel.app/${params.dataId}/${index}/${item}" style='color: blue; margin:0; cursor:pointer'>(&#8209;${criteria.variable[item]['values'][0]})</a>`)
 			  } else if(criteria.variable[item].type == 'indicator') {
-				modified = modified.replace(item, `<a href="https://fitpage-revised.vercel.app${params.dataId}/${index}/${item}" style='color: blue; margin:0; cursor:pointer'>(&#8209;${criteria.variable[item]['default_value']})</a>`)
+				modified = modified.replace(item, `<a href="https://fitpage-revised.vercel.app/${params.dataId}/${index}/${item}" style='color: blue; margin:0; cursor:pointer'>(&#8209;${criteria.variable[item]['default_value']})</a>`)
 			  }
 			})
 			component = (<p style={{display:'flex'}} dangerouslySetInnerHTML={{__html:modified}}></p>)
